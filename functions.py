093c75757c380986bfec347e7deb649fcdf65137
parkss0612 <seongsub344@gmail.com>
2025-05-13 17:28:45 +0900

    functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,3 @@
-def add(a, b):
-    """Add two numbers."""
-    return a + b
-
-def minus(a, b):
-    """Subtract b from a."""
-    return a - b
+def root(a):
+    """Return the square root of a."""
+    return a ** 0.5
