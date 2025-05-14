2ac9598dff2a8436ac37e61f34ab62252f14e7c9
parkss0612 <seongsub344@gmail.com>
2025-05-14 12:06:12 +0900

    functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import cmath
+
 def root(a):
-    """Return the square root of a."""
-    return a ** 0.5
+    """Return the square root of a. Supports complex results for negative inputs."""
+    return cmath.sqrt(a)
+
