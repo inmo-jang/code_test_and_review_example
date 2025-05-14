47809f754d8a33eab417e91a52db1c00be9eeead
son-07 <yongho0203@naver.com>
2025-05-14 11:41:22 +0900

    functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,4 @@
-def add(a, b):
-    """Add two numbers."""
-    return a + b
-
-def minus(a, b):
-    """Subtract b from a."""
-    return a - b
+def divide(a, b):
+    if b ==0:
+        return '0으로 나눌수 없음.'
+    return a / b
